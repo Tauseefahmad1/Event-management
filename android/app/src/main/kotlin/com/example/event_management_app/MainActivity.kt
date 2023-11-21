88c75c0e965a48bb0b9b729a88b3b55a8c874ad9
Tauseefahmad1 <118618795+Tauseefahmad1@users.noreply.github.com>
2023-11-21 13:11:50 +0500

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/event_management_app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.event_management_app
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterFragmentActivity() {
+}
